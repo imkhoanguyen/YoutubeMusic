32b7124a1f13144f7710d1cd40f44f6be8f4d247
Khoa <ankhoa.dev@gmail.com>
2024-01-10 12:02:31 +0700

    Add project files.

---
+++ YoutubeMusic/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace YoutubeMusic.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
